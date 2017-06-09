7e63cac0904888ea6ef744eb08eb6e95c5739432
blackknight75 <devbox4714@gmail.com>
2017-06-09 10:25:04 -0600

    precompile for heroku

--- public/assets/application-fdbb6c008f27adc9f5da4ffc94397511511391b51dc7b83e0094a08c20125003.js
+++ public/assets/application-fdbb6c008f27adc9f5da4ffc94397511511391b51dc7b83e0094a08c20125003.js
@@ -12314,6 +12314,7 @@ function checkDatabase(){
   $.ajax({
     method: "GET",
     url: "https://boiling-woodland-88175.herokuapp.com/api/v1/links"
+    // url: "/api/v1/links"
   }).then(function(links){
     var linkCount = 0
     if(links.length == linkCount){
